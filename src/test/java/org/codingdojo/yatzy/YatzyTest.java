2f073d0a0796cbb121cb70e283c763dfc7a5ebf3
briquet9 <88289097+briquet9@users.noreply.github.com>
2024-05-29 16:53:58 +0200

    refactor: tests mutualized + some redondant tests removed

--- src/test/java/org/codingdojo/yatzy/YatzyTest.java
+++ src/test/java/org/codingdojo/yatzy/YatzyTest.java
@@ -6,142 +6,44 @@ import org.junit.jupiter.params.provider.CsvSource;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
-public class YatzyTest {
-
-    // Upper section
+class YatzyTest {
 
     @ParameterizedTest
     @CsvSource({
-        "1, 1, 2, 3, 4, 5",
-        "2, 1, 2, 1, 4, 5",
-        "0, 6, 2, 2, 4, 5",
-        "4, 1, 2, 1, 1, 1"})
-    public void testOnes(int expected, int d1, int d2, int d3, int d4, int d5) {
-        assertEquals(expected, YatziManager.computeScore(Category.ONES, new Combination(d1, d2, d3, d4, d5)));
-    }
-
-    @ParameterizedTest
-    @CsvSource({
-        "4, 1, 2, 3, 2, 6",
-        "10, 2, 2, 2, 2, 2"})
-    public void testTwos(int expected, int d1, int d2, int d3, int d4, int d5) {
-        assertEquals(expected, YatziManager.computeScore(Category.TWOS, new Combination(d1, d2, d3, d4, d5)));
-    }
-
-    @ParameterizedTest
-    @CsvSource({
-        "6, 1, 2, 3, 2, 3",
-        "12, 2, 3, 3, 3, 3"})
-    public void testThrees(int expected, int d1, int d2, int d3, int d4, int d5) {
-        assertEquals(expected, YatziManager.computeScore(Category.THREES, new Combination(d1, d2, d3, d4, d5)));
-    }
-
-    @ParameterizedTest
-    @CsvSource({
-        "12, 4, 4, 4, 5, 5",
-        "8, 4, 4, 5, 5, 5",
-        "4, 4, 5, 5, 5, 5"})
-    public void testFours(int expected, int d1, int d2, int d3, int d4, int d5) {
-        assertEquals(expected, YatziManager.computeScore(Category.FOURS, new Combination(d1, d2, d3, d4, d5)));
-    }
-
-    @ParameterizedTest
-    @CsvSource({
-        "10, 4, 4, 4, 5, 5",
-        "15, 4, 4, 5, 5, 5",
-        "20, 4, 5, 5, 5, 5"})
-    public void testFives(int expected, int d1, int d2, int d3, int d4, int d5) {
-        assertEquals(expected, YatziManager.computeScore(Category.FIVES, new Combination(d1, d2, d3, d4, d5)));
-    }
-
-    @ParameterizedTest
-    @CsvSource({
-        "0, 4, 4, 4, 5, 5",
-        "6, 4, 4, 6, 5, 5",
-        "18, 6, 5, 6, 6, 5"})
-    public void testSixes(int expected, int d1, int d2, int d3, int d4, int d5) {
-        assertEquals(expected, YatziManager.computeScore(Category.SIXES, new Combination(d1, d2, d3, d4, d5)));
-    }
-
-
-    // Lower section
-
-    @ParameterizedTest
-    @CsvSource({
-        "6, 3, 4, 3, 5, 6",
-        "10, 5, 3, 3, 3, 5",
-        "12, 5, 3, 6, 6, 5"})
-    public void testOnePairs(int expected, int d1, int d2, int d3, int d4, int d5) {
-        assertEquals(expected, YatziManager.computeScore(Category.ONE_PAIR, new Combination(d1, d2, d3, d4, d5)));
-    }
-
-    @ParameterizedTest
-    @CsvSource({
-        "16, 3, 3, 5, 4, 5",
-        "16, 3, 3, 5, 5, 5",
-        "0, 3, 3, 3, 3, 1"})
-    public void testTwoPairs(int expected, int d1, int d2, int d3, int d4, int d5) {
-        assertEquals(expected, YatziManager.computeScore(Category.TWO_PAIRS, new Combination(d1, d2, d3, d4, d5)));
-    }
-
-    @ParameterizedTest
-    @CsvSource({
-        "9, 3, 3, 3, 4, 5",
-        "15, 5, 3, 5, 4, 5",
-        "9, 3, 3, 3, 3, 5",
-        "9, 3, 3, 3, 3, 3"})
-    public void testThreeOfAKind(int expected, int d1, int d2, int d3, int d4, int d5) {
-        assertEquals(expected, YatziManager.computeScore(Category.THREE_OF_A_KIND, new Combination(d1, d2, d3, d4, d5)));
-    }
-
-    @ParameterizedTest
-    @CsvSource({
-        "12, 3, 3, 3, 3, 5",
-        "20, 5, 5, 5, 4, 5"})
-    public void testFourOfAKind(int expected, int d1, int d2, int d3, int d4, int d5) {
-        assertEquals(expected, YatziManager.computeScore(Category.FOUR_OF_A_KIND, new Combination(d1, d2, d3, d4, d5)));
-    }
-
-    @ParameterizedTest
-    @CsvSource({
-        "15, 1, 2, 3, 4, 5",
-        "15, 2, 3, 4, 5, 1",
-        "0, 1, 2, 2, 4, 5"})
-    public void testSmallStraight(int expected, int d1, int d2, int d3, int d4, int d5) {
-        assertEquals(expected, YatziManager.computeScore(Category.SMALL_STRAIGHT, new Combination(d1, d2, d3, d4, d5)));
-    }
-
-    @ParameterizedTest
-    @CsvSource({
-        "20, 6, 2, 3, 4, 5",
-        "20, 2, 3, 4, 5, 6",
-        "0, 1, 2, 2, 4, 5"})
-    public void testLargeStraight(int expected, int d1, int d2, int d3, int d4, int d5) {
-        assertEquals(expected, YatziManager.computeScore(Category.LARGE_STRAIGHT, new Combination(d1, d2, d3, d4, d5)));
-    }
-
-    @ParameterizedTest
-    @CsvSource({
-        "18, 6, 2, 2, 2, 6",
-        "0, 2, 3, 4, 5, 6",
-        "0, 4, 4, 4, 4, 4"})
-    public void testFullHouse(int expected, int d1, int d2, int d3, int d4, int d5) {
-        assertEquals(expected, YatziManager.computeScore(Category.FULL_HOUSE, new Combination(d1, d2, d3, d4, d5)));
-    }
-
-    @ParameterizedTest
-    @CsvSource({
-        "15, 2, 3, 4, 5, 1",
-        "16, 3, 3, 4, 5, 1"})
-    public void testChance(int expected, int d1, int d2, int d3, int d4, int d5) {
-        assertEquals(expected, YatziManager.computeScore(Category.CHANCE, new Combination(d1, d2, d3, d4, d5)));
-    }
-
-    @ParameterizedTest
-    @CsvSource({
-        "50, 4, 4, 4, 4, 4",
-        "0, 6, 6, 6, 6, 3"})
-    public void testYatzy(int expected, int d1, int d2, int d3, int d4, int d5) {
-        assertEquals(expected, YatziManager.computeScore(Category.YATZY, new Combination(d1, d2, d3, d4, d5)));
+        "0, ONES, 6, 2, 2, 4, 5",
+        "1, ONES, 1, 2, 3, 4, 5",
+        "3, ONES, 1, 1, 1, 4, 5",
+        "4, TWOS, 1, 2, 3, 2, 6",
+        "12, THREES, 2, 3, 3, 3, 3",
+        "12, FOURS, 4, 4, 4, 5, 5",
+        "15, FIVES, 4, 4, 5, 5, 5",
+        "18, SIXES, 6, 5, 6, 6, 5",
+        "0, ONE_PAIR, 1, 2, 3, 4, 5",
+        "10, ONE_PAIR, 5, 3, 3, 3, 5",
+        "12, ONE_PAIR, 5, 3, 6, 6, 5",
+        "16, TWO_PAIRS, 3, 3, 5, 4, 5",
+        "16, TWO_PAIRS, 3, 3, 5, 5, 5",
+        "0, TWO_PAIRS, 3, 3, 3, 3, 1",
+        "0, THREE_OF_A_KIND, 3, 3, 1, 4, 5",
+        "15, THREE_OF_A_KIND,5, 3, 5, 4, 5",
+        "15, THREE_OF_A_KIND,5, 5, 5, 4, 5",
+        "12, FOUR_OF_A_KIND, 3, 3, 3, 3, 5",
+        "20, FOUR_OF_A_KIND, 5, 5, 5, 4, 5",
+        "15, SMALL_STRAIGHT, 1, 2, 3, 4, 5",
+        "0, SMALL_STRAIGHT, 2, 3, 4, 5, 6",
+        "0, SMALL_STRAIGHT, 1, 2, 2, 4, 5",
+        "20, LARGE_STRAIGHT, 6, 2, 3, 4, 5",
+        "0, LARGE_STRAIGHT, 1, 2, 2, 4, 5",
+        "18, FULL_HOUSE, 6, 2, 2, 2, 6",
+        "0, FULL_HOUSE, 2, 3, 4, 5, 6",
+        "0, FULL_HOUSE, 2, 2, 4, 5, 6",
+        "0, FULL_HOUSE, 4, 4, 4, 4, 4",
+        "16, CHANCE,  3, 3, 4, 5, 1",
+        "50, YATZY, 4, 4, 4, 4, 4",
+        "0, YATZY, 6, 6, 6, 6, 3"
+    })
+    void testYatzy(int expected, String categoryName, int d1, int d2, int d3, int d4, int d5) {
+        Category category = Category.valueOf(categoryName);
+        assertEquals(expected, YatziManager.computeScore(category, new Combination(d1, d2, d3, d4, d5)));
     }
 }
